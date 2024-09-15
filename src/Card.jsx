5b2acba1934244cc3e23b20b0c166d892f099761
Jahaan Pardhanani <kidflash2jahaan@gmail.com>
2024-09-15 11:18:23 -0700

    React Props Challenge

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,19 +1,16 @@
 export default function Card(props) {
     return (<div className="card">
-            <div className="top">
-                {/* eslint-disable-next-line react/prop-types */}
-                <h2 className="name">{props.name}</h2>
-                <img className="circle-img"
-                     {/* eslint-disable-next-line react/prop-types */}
-                     src={props.imgURL}
-                     alt="avatar_img"
-                />
-            </div>
-            <div className="bottom">
-                {/* eslint-disable-next-line react/prop-types */}
-                <p className="info">{props.phone}</p>
-                {/* eslint-disable-next-line react/prop-types */}
-                <p className="info">{props.email}</p>
-            </div>
-        </div>)
+        <div className="top">
+            {/* eslint-disable-next-line react/prop-types */}
+            <h2 className="name">{props.name}</h2>
+            {/* eslint-disable-next-line react/prop-types */}
+            <img className="circle-img" src={props.imgURL} alt="avatar_img"/>
+        </div>
+        <div className="bottom">
+            {/* eslint-disable-next-line react/prop-types */}
+            <p className="info">{props.phone}</p>
+            {/* eslint-disable-next-line react/prop-types */}
+            <p className="info">{props.email}</p>
+        </div>
+    </div>)
 }
